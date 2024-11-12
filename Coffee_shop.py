c83b42ff907127991a38bd21a157859bdeff2f80
CodeMavrick5 <martin.mayer2008@gmail.com>
2024-11-13 01:25:33 +0200

    Add files via upload

---
+++ Coffee_shop.py
@@ -0,0 +1,8 @@
+print("welcome to Martin's coffee shop\n")
+name =input("what is your name\n")
+print("\nHello " +name+ " welcome to our coffee shop " )
+menu= "black coffee,latte,espresso,ice coffee"
+order=input("\n\nwhat would you like to order here is what we are serving today\n" +menu+ "\n")
+sugar=input("\ndo you want with sugar or no sugar\n")
+extra=input ("Do you want anythying on the side we have all kinds of dounts \n")
+print("sounds good " +name+ " we will have your " + order + " with " + sugar + " and " +extra+" ready for you  in just a moment")

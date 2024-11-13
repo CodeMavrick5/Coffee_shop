3938dfc502ecd4b24a82be2de4a58400c2180700
CodeMavrick5 <martin.mayer2008@gmail.com>
2024-11-13 23:28:57 +0200

    Coffee_shop.py

--- Coffee_shop.py
+++ Coffee_shop.py
@@ -1,18 +1,40 @@
 print("welcome to Martin's coffee shop\n")
+
 name =input("what is your name\n")
-print("\nHello " +name+ " welcome to our coffee shop " )
+
+if name== "Martin":
+  print("welcome Martin you are the owner of this shop everything for you is for free")
+else:
+ print("\nHello " +name+ " welcome to our coffee shop " )
+
 menu= "black coffee,latte,espresso,ice coffee"
+
+
 order=input("\n\nwhat would you like to order here is what we are serving today\n" +menu+ "\n")
-quantity=input("how many do you want?\n")
+
 if order== "ice coffee":
    price = 9
 elif order =="espresso":
     price =7
+elif order =="black coffee":
+    price =3
 elif order =="latte":
     cream= input("Do you want whipped cream?\n")
     if cream == "yes" :
     	price = 8
     else : price = 5
-   
+else : 
+ print ("sorry we don't have that here")
+ exit()
+ 
+ 
+quantity=input("how many do you want?\n")
+
+
 total= price* int(quantity)
-print("sounds good " +name+ " the total will be "+str(total)+"EGP and we will have your " + quantity+" "+order + " ready for you  in just a moment")
+
+if name=="Martin":
+  print("okay the "+quantity+" " +order+" will be for free and will be ready in just a moment")
+  
+else:
+ print("sounds good " +name+ " the total will be "+str(total)+"EGP and we will have your " + quantity+" "+order + " ready for you  in just a moment")
